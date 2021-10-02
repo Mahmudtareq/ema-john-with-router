ef86a671d0ca4eea7afa81bfc7c608cb9fefc40e
Mahmudtareq <tareqlpu5@gmail.com>
2021-10-02 08:57:54 +0600

    completete project

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>404</h1>
+            <p>This is not a valid  page </p>
+
+        </div>
+    );
+};
+
+export default NotFound;
+
--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,9 +1,52 @@
 import React from 'react';
 
+import useProducts from '../../Hooks/useProducts';
+import useCart from '../../Hooks/useCart';
+import Cart from '../Cart/Cart';
+import ItemReview from '../ItemReview/ItemReview';
+import { clearTheCart, removeFromDb } from '../../utilities/fakedb';
+import { useHistory } from 'react-router';
+// import { getStoredCart } from '../../utilities/fakedb';
+
+
 const OrderReview = () => {
+    const [products] = useProducts();
+    const [cart, setCart] = useCart(products);
+    const history = useHistory();
+
+    const handleRemove = key => {
+        const newCart = cart.filter(product => product.key !== key);
+        setCart(newCart);
+        removeFromDb(key);
+
+    }
+    const handlePlaceOrder = () => {
+        history.push('/placeorder')
+        setCart([]);
+        clearTheCart();
+
+    }
+
     return (
-        <div>
-            <h2>This is Order Review</h2>
+        <div className="shop-container">
+            <div className="product-container">
+                {
+                    cart.map(product => <ItemReview
+                        key={product.key}
+                        product={product}
+                        handleRemove={handleRemove}
+                    ></ItemReview>)
+                }
+
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}>
+                    <button onClick={handlePlaceOrder} className="btn-regular">place order</button>
+
+                </Cart>
+
+            </div>
+
         </div>
     );
 };
